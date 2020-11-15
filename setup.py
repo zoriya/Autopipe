e42ad0402714fc9846f13099197c00b97eb4fce7
Zoe Roux <zoe.roux@sdg.moe>
2020-11-15 19:31:32 +0100

    Fixing pip's build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="autopipe-AnonymusRaccoon",
+    name="autopipe",
     version="0.0.1",
     author="Tristan Roux",
     author_email="tr.tristan-roux@outlook.fr",
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AnonymusRaccoon/Autopipe",
+    scripts=["./bin/autopipe"],
     packages=setuptools.find_packages(),
+    install_requires=["requests", "feedparser"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
